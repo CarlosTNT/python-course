715d6255fc4a85e10a3698657257b469437608f3
Carlos Andres <carlosandrestnt@gmail.com>
2022-10-07 13:34:07 -0500

    Agregado datatype.pt

---
+++ datatype.py
@@ -0,0 +1,33 @@
+# Strings
+print("hello world")
+
+# Intenger
+print(10)
+
+# Float
+print(30.1)
+
+# Boolean
+True
+False
+print(False)
+
+# list
+[10,20,30]
+["hello","bye",]
+
+#tuples
+(10,20,55)
+
+# Dictionaries
+#Clave y valor
+dictio={
+    "name":"Ryan",
+    "lastname":"Sierra",
+    "nickname":"CarlosTnT"
+}
+print(type({
+    "name":"Ryan",
+    "lastname":"Sierra",
+    "nickname":"CarlosTnT"
+}))
