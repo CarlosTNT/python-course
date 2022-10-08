3ceb6d0dd74664fbb031fd6113d3fe3d4e0b87bb
Carlos Andres <carlosandrestnt@gmail.com>
2022-10-08 08:36:57 -0500

    Agregado numbers.py

---
+++ numbers.py
@@ -0,0 +1,24 @@
+# numbers
+
+from re import A
+
+
+print(type(9))
+
+# mathematical operations
+
+a=3
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+
+
+age = input("Insert your age: ")
+print(type(age))
+new_age = int(age) + 5
+print(age)
+print(new_age)
