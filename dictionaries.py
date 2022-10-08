26240e73a45ab09c2c6f6a4fd649aaf777b91baa
Carlos Andres <carlosandrestnt@gmail.com>
2022-10-08 11:44:53 -0500

    agregado dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,27 @@
+# dictionaries
+
+cart = {
+    "name": "book",
+    "price": 4.99,
+    "quantify":3
+}
+
+person = {
+    "firts_name": "carlos",
+    "last_name": "sierra"
+}
+
+print(type(person))
+
+print(person.keys())
+print(person.items())
+
+person.clear()
+print(person)
+
+products = [
+    {"name": "book1","price":10.99},
+    {"name": "book2","price":12.99}
+]
+
+print(products)
