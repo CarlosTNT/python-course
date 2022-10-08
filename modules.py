48db64d66684c0fadcb1d6483024fc4a68d12b4c
Carlos Andres <carlosandrestnt@gmail.com>
2022-10-08 13:32:18 -0500

    agregado modules.py

---
+++ modules.py
@@ -0,0 +1,15 @@
+# modules
+
+# own mules
+# thirdy party modules
+# python modules
+
+import datetime
+
+print(datetime.date.today())
+print(datetime.timedelta(hours=50000))
+
+from datetime import timedelta,date
+
+print(date.today())
+print(timedelta(minutes=180))
