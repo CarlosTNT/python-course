f8f546ccd61dea5b4fa67218b8b1957c9ddc3728
Carlos Andres <carlosandrestnt@gmail.com>
2022-10-08 13:49:41 -0500

    agregado fmath.py ymodules.py

--- modules.py
+++ modules.py
@@ -5,6 +5,11 @@
 # python modules
 
 import datetime
+import fmath
+
+print(fmath.add(12,7))
+print(fmath.substract(20,10))
+
 
 print(datetime.date.today())
 print(datetime.timedelta(hours=50000))
@@ -13,3 +18,13 @@ from datetime import timedelta,date
 
 print(date.today())
 print(timedelta(minutes=180))
+
+from fmath import substract,add
+
+print(add(45,60))
+print(substract(41,11))
+
+import colorama
+from colorama import Fore,Style
+
+print(Fore.MAGENTA+"Hello world")
