c3de1c9cfe6da518ec66c1f46324b25dbf0dbcc1
Carlos Andres <carlosandrestnt@gmail.com>
2022-10-08 08:19:49 -0500

    Agreragado strings.py

---
+++ strings.py
@@ -0,0 +1,33 @@
+#dir
+myStr = "hello world"
+
+# print(dir(myStr))
+
+print(myStr.upper())
+print(myStr.lower())
+print(myStr.swapcase())
+print(myStr.capitalize())
+
+print(myStr.replace("hello", "bye"))
+
+print(myStr.count("l"))
+
+print(myStr.startswith("hello"))
+
+print(myStr.endswith("hello"))
+
+print(myStr.split())
+print(myStr.split("o"))
+
+print(myStr.find("0"))
+
+print(len(myStr))
+myStr.index("e")
+myStr.isnumeric()
+myStr.isalnum()
+
+print(myStr[0])
+
+myName = "carlos Andres Sierra"
+
+print(f"mi nombre es {myName}")
