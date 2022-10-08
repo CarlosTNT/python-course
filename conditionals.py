beaaa7bc3b69b647f9670dfdaae16c9314d486e7
Carlos Andres <carlosandrestnt@gmail.com>
2022-10-08 12:10:17 -0500

    agregado conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,33 @@
+x = 40
+
+if x < 30:
+    print ("x is less than 30")
+else:
+    print("x is greater than 30")
+
+color = "blue"
+
+if color == "red":
+    print("The color is red")
+elif color == "blue":
+    print("The color is blue")
+else:
+    print("any color")
+
+name = "jhn"
+lastname = "gomez"
+
+if name == "jhon":
+    if lastname == "carter":
+        print("You are Jhon Carter")
+    else:
+        print("You are not Carter")
+else:
+    print("you are not jhon")
+
+if x > 2 and x > 10:
+    print("x is greater tah two and less than or equal to ten")
+if x > 2 or x <= 20:
+    print("x is greater than two or less or equal to twenty")
+if (not(x==y)):
+    print("x not is equal to y")
